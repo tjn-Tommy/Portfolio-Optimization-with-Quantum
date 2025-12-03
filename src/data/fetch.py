4548ad44d2de593f80e350b4f79bd7386cd4a70f
tjn-Tommy <145881628+tjn-Tommy@users.noreply.github.com>
2025-12-03 21:02:04 +0800

    Update the data

--- src/data/fetch.py
+++ src/data/fetch.py
@@ -2,10 +2,12 @@ import yfinance as yf
 import numpy as np
 
 # 1. list of tickers
-tickers = ['NVDA', 'JPM', 'JNJ', 'PG', 'XOM']
-
+hybird_tickers = ['NVDA', 'JPM', 'JNJ', 'PG', 'XOM', 'BA', 'WMT']
+tech_tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META', 'TSLA']
 # 2. download historical data (usually past 1-3 years of closing prices)
-data = yf.download(tickers, start="2022-01-01", end="2025-01-01")
+data = yf.download(hybird_tickers, start="2022-01-01", end="2025-01-01")
+tech_data = yf.download(tech_tickers, start="2022-01-01", end="2025-01-01")
 
 # 3. store data to csv
-data.to_csv('historical_data.csv')
+data.to_csv('hybrid_historical_data.csv')
+tech_data.to_csv('tech_historical_data.csv')
